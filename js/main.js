10aeed74eca650cc18460cde61da49b7a7aeb5a8
wiolettamehiadin <wioletta_k@live.co.uk>
2021-06-01 21:36:26 +0100

    Rozpoczecie pliku

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+document.querySelector('button').onclick = function() {
+let inputvalue = document.getElementById('name1').value
+document.getElementById('name').textContent = inputvalue;       
+}  
+
+document.querySelector('button').onclick = function() {
+let inputvalue = document.getElementById('surname1').value
+document.getElementById('surname').textContent = inputvalue;
+}        
+
+
+document.querySelector('button').onclick = function() {
+let inputvalue = document.getElementById('phone1').value
+document.getElementById('phone').textContent = inputvalue;             
+}

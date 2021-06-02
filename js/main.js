31fdcd8599b4567ebbdf183ed19ae292eda92e20
wiolettamehiadin <wioletta_k@live.co.uk>
2021-06-02 16:58:34 +0100

    Poprawka

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
+document.querySelector('button').onclick = function () {
+    let inputName = document.getElementById('name1').value;
+    document.getElementById('name').textContent = inputName;
+    let inputSurname = document.getElementById('surname1').value;
+    document.getElementById('surname').textContent = inputSurname;
+    let inputPhone = document.getElementById('phone1').value;
+    document.getElementById('phone').textContent = inputPhone;
 
-document.querySelector('button').onclick = function() {
-let inputvalue = document.getElementById('name1').value
-document.getElementById('name').textContent = inputvalue;       
-}  
-
-document.querySelector('button').onclick = function() {
-let inputvalue = document.getElementById('surname1').value
-document.getElementById('surname').textContent = inputvalue;
-}        
-
-
-document.querySelector('button').onclick = function() {
-let inputvalue = document.getElementById('phone1').value
-document.getElementById('phone').textContent = inputvalue;             
 }
